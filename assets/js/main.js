f3e7ced573a7537942c45c8b0a3a0e4db53b2d6f
docongban <banto2172001@gmail.com>
2022-02-07 18:15:32 +0700

    highlight

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Start slider
 var slides = document.querySelectorAll('.slider__background')
 var preButton = document.querySelector('.slider__btn--left')
 var nextButton = document.querySelector('.slider__btn--right')
@@ -34,3 +35,39 @@ function sliderBehavior(e){
     // add btn mới
     slides[index].classList.add('active')
 }
+// End slider
+
+
+// Start choose color
+var colors = document.querySelectorAll('.sample__product--color-item')
+var products = document.querySelectorAll('.sample__product')
+
+colors.forEach(function(color){
+    color.onclick = function(){
+        document.querySelectorAll('.sample__product--color-item.active').forEach(function(e){
+            e.classList.remove('active')
+        })
+        color.classList.add('active')
+    }
+})
+// End choose color
+
+
+// Start tab pane highlight
+var tabs = document.querySelectorAll('.sample__link--item')
+var panes = document.querySelectorAll('.sample__item')
+
+const tabActive = document.querySelector('.sample__link--item.active')
+
+tabs.forEach(function(tab,index){
+    const pane = panes[index]
+    
+    tab.onclick= function(){
+        document.querySelector('.sample__link--item.active').classList.remove('active')
+        document.querySelector('.sample__item.active').classList.remove('active')
+
+        tab.classList.add('active')
+        pane.classList.add('active')
+    }
+})
+// End tab pane highlight
