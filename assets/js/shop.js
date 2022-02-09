3244ad3854120c89a112876f31af807e385c20d7
docongban <banto2172001@gmail.com>
2022-02-09 11:10:24 +0700

    complete slidebar

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,9 +1,25 @@
 // Start slidebar
+
 var slideBars = document.querySelectorAll('.slidebar__funtion__item')
+var titleSlideBars = document.querySelectorAll('.slidebar__funtion__item--header')
 
 slideBars.forEach(function(slideBar,index) {
-    slideBar.onclick = function(){
+    var titleSlideBar = titleSlideBars[index]
+    titleSlideBar.onclick = function(){
         slideBar.classList.toggle('active')
     }
 })
+
+// click chosse size
+var sizes = document.querySelectorAll('.slidebar__funtion__item:nth-child(4) .slidebar__funtion__item--list__item')
+
+sizes.forEach(function(size){
+    size.onclick = function(){
+        
+        document.querySelector('.slidebar__funtion__item:nth-child(4) .slidebar__funtion__item--list__item.active').classList.remove('active')
+
+        size.classList.add('active')
+    }
+})
+
 // End slidebar
