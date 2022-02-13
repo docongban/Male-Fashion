90d8b2530ded71e6f9c7b5051aebe929c7aabe00
docongban <banto2172001@gmail.com>
2022-02-13 23:07:29 +0700

    responsive menu nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,3 +108,34 @@ var x = setInterval(function() {
 // End countdown time
 
 
+// Start menu mobile
+var menuBtn = document.querySelector('.header__mobile--menu')
+var overlay = document.querySelector('.overlay')
+var menuMobile = document.querySelector('.mobile__menu_background')
+
+menuBtn.onclick = function() {
+    overlay.classList.add('active')
+    menuMobile.classList.add('active')
+}
+
+overlay.onclick = function() {
+    setTimeout(function() {
+        overlay.classList.remove('active')
+    },500)
+    menuMobile.classList.remove('active')
+}
+// End menu mobile
+
+
+// Start list menu
+document.querySelector('.mobile__menu__option--money').onclick = function() {
+    document.querySelector('.mobile__menu__option--money-list').classList.toggle('active')
+}
+
+var listMenuBtn = document.querySelector('.mobile__menu__navbar--btn')
+var list = document.querySelector('.mobile__menu__navbar--list')
+
+listMenuBtn.onclick = function(){
+    list.classList.toggle('active')
+}
+// End list menu
